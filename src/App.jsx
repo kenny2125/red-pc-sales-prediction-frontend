64d593c71a91ffac3c64bcc8fb22e089d32b11e0
kenny2125 <johnkennyreyes21@gmail.com>
2025-03-21 01:23:59 +0800

    Added colors and finalized styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <>
         <div >
-          <h1 className='text-8xl-anton'> 
+          <h1 className="text-8xl-anton text-primary "> 
             RED PC PH
           </h1>
-          <p className='text-base-inter'> 
+          <p className='text-lg-inter'> 
               Check out
           </p>
         </div>
