4f7643bcf2fb8c45e73291d58cbcea6ba28ac4e9
kenny2125 <johnkennyreyes21@gmail.com>
2025-03-23 01:47:40 +0800

    Improve Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { UserPen } from "lucide-react";
 import { ScrollText } from "lucide-react";
 import { ShoppingCart } from "lucide-react";
+import { Search } from "lucide-react";
 
 type UserRole = "guest" | "customer" | "admin";
 
@@ -18,10 +19,11 @@ interface User {
 
 export default function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isAdmin, setAdmin] = useState<boolean>(false);
   const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const userType: UserRole = "customer";
+    const userType: UserRole = "admin";
 
     switch (userType) {
       case "customer":
@@ -32,6 +34,7 @@ export default function Header() {
           role: "customer",
         });
         setIsLoggedIn(true);
+        setAdmin(false);
         break;
       case "admin":
         setCurrentUser({
@@ -41,10 +44,13 @@ export default function Header() {
           role: "admin",
         });
         setIsLoggedIn(true);
+        setAdmin(true);
         break;
       default:
         setIsLoggedIn(false);
+        setAdmin(false);
         setCurrentUser(null);
+
         break;
     }
   }, []);
@@ -55,18 +61,39 @@ export default function Header() {
   };
 
   return (
-    <div className="w-full h-[122px] inline-flex justify-between items-center">
-      <div className="inline-flex justify-center items-center">
+    <div className="w-full h-[8rem] inline-flex justify-between items-center">
+      <div className="inline-flex justify-center items-center gap-2">
         <img className="w-[76.13px] h-[76px]" src={RedPcLogo} />
         <div className="text-4xl  font-['Anton']">RED PC</div>
       </div>
 
-      <div className="flex justify-start items-center gap-[18px]">
-        <div className="flex w-[520px] h-[53px] items-center gap-2">
-          <Input type="email" placeholder="Search" />
-          <Button type="submit">Search</Button>
+      {currentUser?.role == "admin" ? (
+        <div className="inline-flex align-middle justify-center gap-12">
+          <Button className="text-foreground text-1xl" variant="link">
+            Dashboard
+          </Button>
+          <Button className="text-foreground text-1xl" variant="link">
+            Inventory
+          </Button>
+          <Button className="text-foreground text-1xl" variant="link">
+            Sales
+          </Button>
+          <Button className="text-foreground text-1xl" variant="link">
+            Orders
+          </Button>
         </div>
-      </div>
+      ) : (
+        <div className="flex justify-start items-center gap-[18px]">
+          <div className="flex w-[520px] h-[53px] items-center gap-2">
+            <Input type="email" placeholder="Search" />
+            <Button type="submit">
+              <Search />
+              Search
+            </Button>
+          </div>
+        </div>
+      )}
+
       <div>
         {/* <Button variant="link" type="submit">About Us</Button> */}
 
@@ -74,8 +101,8 @@ export default function Header() {
           <>
             <div className="inline-flex items-center gap-2">
               <div className="inline-block align-middle">
-              <h1>Welcome Back</h1>
-              <p>{currentUser?.name}</p>
+                <h1>Welcome Back</h1>
+                <p>{currentUser?.name}</p>
               </div>
               <div className="inline-flex items-center gap-2">
                 <ScrollText size={40} className="text-primary" />
