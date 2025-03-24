53006175971b42303507c6b8684a897d615275da
Kael1817 <obidos.michael.cautiber@gmail.com>
2025-03-24 23:20:00 +0800

    first section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,30 @@
-import { UserProvider } from "../contexts/UserContext";
-import Header from '../components/Header'
-import Footer from "../components/Footer"
-import PackageCard from '@/components/cards/package-card'
-import ProductCard from '@/components/cards/product-card'
-import UserCard from '@/components/cards/user-card'
-import  {LineChartView}  from "@/components/charts/LineChartMultiple"
-import { PieChartDonut } from "@/components/charts/PieChartDonut"
-import { TableDemo } from '@/components/Table'
-import { LineChartInteractive } from '@/components/charts/LineChartInterative'
-
+import gif from '../assets/home-gif.gif'
+import PackageSet from '../assets/package-assets/package-set.png'
+import { Button } from '@/components/ui/button';
+import { CircleArrowRight } from 'lucide-react';
 
 function Home() {
-  return (<>
-      
-        <div className='w-lg'>
-        <LineChartView />
-        </div>        
-        <PackageCard />
-        <ProductCard />
-        <UserCard />
-        <PieChartDonut/>
-        <TableDemo/>
-        <div className='w-full'>
-        <LineChartInteractive/>
-        </div>
+  return (
+    <section 
+      style={{
+        backgroundImage: `url(${gif})`,
         
-        </>
-
-  )
+      }} 
+      className="w-full h-screen bg-cover bg-center bg-no-repeat opacity-40 shadow-[inset_0px_4px_100px_100px_rgba(0,0,0,0.4)] backdrop-blur-[6.8px] px-[110px]"
+    >
+      <div className='flex items-center gap-[50px]'>
+        <div>
+          <h1 className='text-7xl font-bold mb-[10px]'>Empowering Your <br /> Digital Experience</h1><br />
+          <p className='font-bold text-2xl'>Providing Quality Computers Across the Philipines.</p>
+          <p className='font-bold text-2xl'>Starting at <span className='text-2xl text-amber-300'>₱17,399</span> per computer set</p>
+          <Button className='font-bold text-xl mt-[35px]'>Shop Now <CircleArrowRight></CircleArrowRight></Button>
+        </div>
+        <div>
+          <img src={PackageSet} alt="Package Set Image" className='w-[581px] ' />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
