d01843525e544448696c9d3d1be8951a14e5a7b2
kenny2125 <johnkennyreyes21@gmail.com>
2025-03-24 15:08:12 +0800

    Initial Success Payment

--- src/components/dialogs/CartDialog.tsx
+++ src/components/dialogs/CartDialog.tsx
@@ -33,6 +33,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import ProductCard from "../cards/product-card";
 
 const invoices = [
   {
@@ -59,127 +60,145 @@ const invoices = [
 ];
 
 export function CartDialog() {
+  const [paid, isPaid] = useState<boolean>(false);
+
+  function payment(params:type) {
+    isPaid(true);
+  }
+  
+
   return (
-    <Dialog>
+    <Dialog>      
       <DialogTrigger asChild>
         <ShoppingCart size={40} className="text-primary" />
       </DialogTrigger>
 
-      <DialogContent className="flex flex-col sm:max-w-[1570px] min-h-[577px]">
+      <DialogContent className="flex flex-col  sm:max-w-[1570px] min-h-[577px]">
         <DialogHeader>
           <div className="flex flex-row align-middle items-center gap-">
             <ShoppingCart size={40} className="text-primary" />
             <DialogTitle>My Cart</DialogTitle>
           </div>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+          (Papalitan neto hehe) Make changes to your profile here. Click save when you're done. 
           </DialogDescription>
         </DialogHeader>
-
-        <div className="flex flex-row gap-8 justify-between">
-          <div className="flex-1 overflow-x-auto">
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[50px]"></TableHead>
-                  <TableHead className="w-[100px]"></TableHead>
-                  <TableHead>Product Name</TableHead>
-                  <TableHead>Quantity</TableHead>
-                  <TableHead className="text-right">Amount</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {invoices.map((invoice) => (
-                  <TableRow key={invoice.id} className="align-middle">
-                    <TableCell className="vertical-align-middle">
-                      <Checkbox />
-                    </TableCell>
-                    <TableCell>
-                      <img
-                        src="https://placehold.co/100"
-                        alt=""
-                        className="w-full"
-                      />
-                    </TableCell>
-                    <TableCell className="font-medium">
-                      {invoice.productName}
-                    </TableCell>
-                    <TableCell className="text-center">
-                      {invoice.quantity}
-                    </TableCell>
-                    <TableCell className="text-right">
-                      ₱{invoice.price.toLocaleString()}
-                    </TableCell>
+        {paid == false ? (
+          <div className="flex flex-row gap-8 justify-between">
+            <div className="flex-1 overflow-x-auto">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[50px]"></TableHead>
+                    <TableHead className="w-[100px]"></TableHead>
+                    <TableHead>Product Name</TableHead>
+                    <TableHead>Quantity</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-
-          <div className="flex flex-col gap-2 justify-start align-bottom h-full">
-            <h1>Customer Information</h1>
-            <div>
-              <h1>Order# </h1>
-              <p></p>
+                </TableHeader>
+                <TableBody>
+                  {invoices.map((invoice) => (
+                    <TableRow key={invoice.id} className="align-middle">
+                      <TableCell className="vertical-align-middle">
+                        <Checkbox />
+                      </TableCell>
+                      <TableCell>
+                        <img
+                          src="https://placehold.co/100"
+                          alt=""
+                          className="w-full"
+                        />
+                      </TableCell>
+                      <TableCell className="font-medium">
+                        {invoice.productName}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {invoice.quantity}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        ₱{invoice.price.toLocaleString()}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             </div>
-            
-            <div className="flex flex-row gap-12 justify-between">
-              <div className="flex flex-row gap-2">
-                <Label>Payment Method</Label>
-                <Select>
-                  <SelectTrigger className="w-[200px]">
-                    <SelectValue placeholder="Select a Payment Method" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="cod">Cash on Delivery</SelectItem>
-                    <SelectItem value="gcash">GCASH</SelectItem>
-                    <SelectItem value="paymaya">PAYMAYA</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className="flex flex-row gap-2">
-                <Label>Pickup Method</Label>
-                <Select>
-                  <SelectTrigger className="w-[200px]">
-                    <SelectValue placeholder="Select a Pickup Method" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="store">Store Pickup</SelectItem>
-                    <SelectItem value="delivery">Delivery</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            </div>
-            <div className="flex flex-row gap-8 justify-between ">
-              <div className="flex flex-row gap-4">
-                <h1>Customer Name</h1>
-                <p>John Kenny Reyes</p>
-              </div>
-
-              <div className="flex flex-row gap-4">
-                <h1>Contact Number</h1>
-                <p>09123456789</p>
-              </div>
-            </div>
-            <div>
-              <h1>Address</h1>
-              <p>Blk 1, Lot 2, San Bandangilid, Novaliches, Quezon City</p>
-            </div>
-            <div className="flex flex-row justify-between align-bottom">
+            <div className="flex flex-col gap-2 justify-start align-bottom h-full">
+              <h1>Customer Information</h1>
               <div>
-                <h1>Order Information</h1>
-                <p>Total Payment</p>
+                <h1>Order# </h1>
+                <p></p>
               </div>
-              ₱
-              {invoices
-                .reduce((sum, invoice) => sum + invoice.price, 0)
-                .toLocaleString()}
+              <div className="flex flex-row gap-12 justify-between">
+                <div className="flex flex-row gap-2">
+                  <Label>Payment Method</Label>
+                  <Select>
+                    <SelectTrigger className="w-[200px]">
+                      <SelectValue placeholder="Select a Payment Method" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="cod">Cash on Delivery</SelectItem>
+                      <SelectItem value="gcash">GCASH</SelectItem>
+                      <SelectItem value="paymaya">PAYMAYA</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="flex flex-row gap-2">
+                  <Label>Pickup Method</Label>
+                  <Select>
+                    <SelectTrigger className="w-[200px]">
+                      <SelectValue placeholder="Select a Pickup Method" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="store">Store Pickup</SelectItem>
+                      <SelectItem value="delivery">Delivery</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+              <div className="flex flex-row gap-8 justify-between ">
+                <div className="flex flex-row gap-4">
+                  <h1>Customer Name</h1>
+                  <p>John Kenny Reyes</p>
+                </div>
+
+                <div className="flex flex-row gap-4">
+                  <h1>Contact Number</h1>
+                  <p>09123456789</p>
+                </div>
+              </div>
+              <div>
+                <h1>Address</h1>
+                <p>Blk 1, Lot 2, San Bandangilid, Novaliches, Quezon City</p>
+              </div>
+              <div className="flex flex-row justify-between align-bottom">
+                <div>
+                  <h1>Order Information</h1>
+                  <p>Total Payment</p>
+                </div>
+                ₱
+                {invoices
+                  .reduce((sum, invoice) => sum + invoice.price, 0)
+                  .toLocaleString()}
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="flex flex-col w-full gap-8 justify-center">
+            <div className="flex justify-center">
+              <h1>Thank you for paying!</h1>
+            </div>
+            <div className="flex flex-row justify-center">
+              <ProductCard/>
+              <ProductCard/>
+              <ProductCard/>
+              <ProductCard/>
+            </div>
+            </div>
+        )}
 
         <DialogFooter>
-          <Button type="submit">Place Order</Button>
+          <Button type="submit" onClick={payment}>Place Order</Button>  
         </DialogFooter>
       </DialogContent>
     </Dialog>
